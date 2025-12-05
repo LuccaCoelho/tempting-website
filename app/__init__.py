e13c4809d374006d9db70a1528ab78a8e7ffb453
LuccaCoelho <viniccius.lucca@gmail.com>
2025-12-05 11:45:59 -0700

    now it is again

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,10 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+        
     migrate.init_app(app, db)
     from .routes import bp
     app.register_blueprint(bp)
